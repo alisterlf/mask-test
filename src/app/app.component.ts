5eec8f6d47af79400c7bd01cb54cce0e7d3ba0c9
Álister Lopes Ferreira <alisterlf@gmail.com>
2019-12-11 19:15:45 -0300

    angular2-text-mask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mask-test';
+export class AppComponent implements OnInit {
+  maskForm: FormGroup;
+  public mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+  public numberMask = createNumberMask({
+    prefix: 'R$',
+    allowDecimal: true,
+    thousandsSeparatorSymbol: '.',
+    decimalSymbol: ','
+  });
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.buildForm();
+  }
+  private buildForm() {
+    this.maskForm = this.fb.group({
+      amount: ''
+    });
+  }
+  convertCurrencyToDecimal(currency: string): number {
+    console.log('currencyWithoutThousandsSeparator', +currency.replace(/[\.R\$]/g, '').replace(/[\,]/g, '.'));
+    return +currency.replace(/[\.R\$]/g, '').replace(/[\,]/g, '.');
+  }
+  submitFund(): void {
+    if (this.maskForm.valid) {
+      this.convertCurrencyToDecimal(this.maskForm.get('amount').value);
+      console.log('this.maskForm.value', this.maskForm.value);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextMaskModule } from 'angular2-text-mask';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TextMaskModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
